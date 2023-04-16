a0f60012ff3eea7db82eb80e32715551be69cb5f
Niharika Harmalkar <niharikaharmalkar@gmail.com>
2023-04-17 00:01:15 +0530

    application-form

--- esd/src/components/ApplicationForm.jsx
+++ esd/src/components/ApplicationForm.jsx
@@ -19,8 +19,12 @@ function MyVerticallyCenteredModal(props) {
         <p>All of your input will be lost.</p>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide} variant="outline-primary">Close</Button>
-        <Button onClick={props.onHide}>Ok</Button>
+        <Button onClick={props.onHide} variant="outline-primary">
+          Close
+        </Button>
+        <a href="/merit-scholarships">
+          <Button onClick={props.onHide}>Ok</Button>
+        </a>
       </Modal.Footer>
     </Modal>
   );
@@ -44,6 +48,7 @@ export default function ApplicationForm() {
           </div>
           <form className="bg-transparent">
             <Container>
+              <h4>Personal Details</h4>
               {/* Name */}
               <div className="form-group  ">
                 <Row>
@@ -241,6 +246,259 @@ export default function ApplicationForm() {
                   </Col>
                 </Row>
               </div>
+              <br />
+              <h4>Bank Details</h4>
+              <br />
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className="col-form-label" for="bank_name">
+                      Bank Name <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={10}>
+                    <div className="">
+                      <input
+                        name="bank_name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Bank Name"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              {/* Account Number */}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className="col-form-label" for="acc_no">
+                      Account Number <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={4}>
+                    <div className="">
+                      <input
+                        name="acc_no"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter Account Number"
+                      />
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={12} md={1}>
+                    <label className="col-form-label" for="branch">
+                      Branch <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={5}>
+                    <div className="">
+                      <input
+                        name="branch"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter Bank Branch name"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              <br />
+              <h4>Income Details</h4>
+              <br />
+              {/* DOB, Age and Gender */}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className=" col-form-label" for="income">
+                      Income <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={4}>
+                    <div className="   ">
+                      <input
+                        name="income"
+                        className="form-control"
+                        type="number"
+                        placeholder="Enter Income"
+                      />
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className=" col-form-label" for="income_proof">
+                      Proof of Income <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={4}>
+                    <div className="   ">
+                      <input
+                        name="income"
+                        className="form-control"
+                        type="number"
+                        placeholder="Attach Proof of Income"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+<br/>
+              <h4>Academic Details</h4>
+<br/>
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className="col-form-label" for="college_name">
+                      Name of College <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={10}>
+                    <div className="">
+                      <input
+                        name="college_name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter College Name"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              {/* UID and Email*/}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className="col-form-label" for="uid">
+                      UID <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={3}>
+                    <div className="">
+                      <input
+                        name="uid"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter UID"
+                      />
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={12} md={1}>
+                    <label className="col-form-label" for="email">
+                      Email <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={6}>
+                    <div className="">
+                      <input
+                        name="email"
+                        className="form-control"
+                        type="email"
+                        placeholder="Enter your college email id"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              {/* DOB, Age and Gender */}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className="    col-form-label" for="degree">
+                      Degree <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={3}>
+                    <div className="">
+                      <Col sm={12} md={12}>
+                        <select className="custom-select">
+                          <option selected="">Choose Degree </option>
+                          <option value={1}>B.E.</option>
+                          <option value={2}>B.Tech</option>
+                          <option value={3}>B.Sc</option>
+                        </select>
+                      </Col>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={12} md={1}>
+                    <label className=" col-form-label" for="gpa">
+                      GPA <span className="compulsory">*</span>
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={3}>
+                    <div className="   ">
+                      <input
+                        name="gpa"
+                        className="form-control"
+                        type="number"
+                        placeholder="Enter Current GPA"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              {/* Achievements */}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={2}>
+                    <label className="col-form-label" for="Achievements">
+                      Achievements
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={10}>
+                    <div className="">
+                      <input
+                        name="Achievements"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter Achievements"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              {/* Extracurricular Activities */}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={3}>
+                    <label
+                      className="col-form-label"
+                      for="Extracurricular_Activities"
+                    >
+                      Extracurricular Activities
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={9}>
+                    <div className="">
+                      <input
+                        name="Extracurricular_Activities"
+                        className="form-control"
+                        type="text"
+                        placeholder="Enter Extracurricular Activities (if any)"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              {/* Letter Of Recommendation */}
+              <div className="form-group  ">
+                <Row>
+                  <Col xs={12} sm={12} md={3}>
+                    <label className="col-form-label" for="lor">
+                      Letter of Recommendation
+                    </label>
+                  </Col>
+                  <Col xs={12} sm={12} md={9}>
+                    <div className="">
+                      <input
+                        name="lor"
+                        className="form-control"
+                        type="text"
+                        placeholder="Attach Letter of Recommendation (if any)"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+
               {/* SUbmit Button */}
               <div className="form-group  ">
                 <Row>
